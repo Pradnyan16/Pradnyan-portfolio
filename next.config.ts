16c0ede50deb53fcd44fa948394cd5542496a207
Developer <dev@example.com>
2026-06-10 22:37:02 +0530

    fix: removed unnecessary ts-expect-error directive to fix Vercel type checking

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // @ts-expect-error ignore custom allowedDevOrigins property
   allowedDevOrigins: ['192.168.1.154', 'http://192.168.1.154:3000'],
 };
 
