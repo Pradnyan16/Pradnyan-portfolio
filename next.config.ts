4c2d37b037fea789f3e813f7016d8abb7fd90d91
Developer <dev@example.com>
2026-06-10 22:34:26 +0530

    fix: added description to ts-expect-error to satisfy strict linting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // @ts-expect-error
+  // @ts-expect-error ignore custom allowedDevOrigins property
   allowedDevOrigins: ['192.168.1.154', 'http://192.168.1.154:3000'],
 };
 
